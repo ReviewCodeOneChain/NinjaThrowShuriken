25a33a657739e5adbb687b99b83769aa4b68ac9b
Hân Phan <hanphan1509@gmail.com>
2022-10-26 12:13:59 +0700

    update project

--- Assets/Scripts/SpinShuriken.cs
+++ Assets/Scripts/SpinShuriken.cs
@@ -29,17 +29,37 @@ public class SpinShuriken : MonoBehaviour
         transform.Rotate(0, 0, -72f);
 
         //V? trí c?a nhân v?t thay ??i theo time
+        //time += Time.deltaTime;
+        //transform.position = startPos + (direction * force * time) + 0.5f * Physics2D.gravity * (time * time);
         time += Time.deltaTime;
-        transform.position = startPos + (direction * force * time) + 0.5f * Physics2D.gravity * (time * time);
+        transform.position = shurikenPos(time);
+
+        float t2 = time + Time.deltaTime;
+
+        Vector2 posFuture = shurikenPos(t2);
+        Vector2 directionFuture = posFuture - (Vector2)transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, directionFuture, Vector2.Distance(transform.position, posFuture));
+        if (hit.collider.transform.CompareTag("Target"))
+        {
+            Destroy(gameObject);
+            Destroy(hit.collider.gameObject);
+            //Debug.Log(hit.collider.name);
+        }
+    }   
+
+    private Vector2 shurikenPos(float t)
+    {
+        return startPos + (direction * force * t) + 0.5f * Physics2D.gravity * (t * t);
     }
 
     //Two objects colliding
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.CompareTag("Target"))
-        {
-            Destroy(gameObject);
-            Destroy(collision.gameObject);
-        }    
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.transform.CompareTag("Target"))
+    //    {
+    //        Destroy(gameObject);
+    //        Destroy(collision.gameObject);
+    //    }    
+    //}
 }
